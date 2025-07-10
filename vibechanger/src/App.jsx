5685746de724b111e954ba1ef5c2895d55ae50dc
subhash2707 <subhashchandrascr8433@gmail.com>
2025-07-10 16:51:57 +0530

    background changer app using React and Tailwind

---
+++ vibechanger/src/App.jsx
@@ -0,0 +1,51 @@
+ import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+ // const [count, setCount] = useState(0)
+//  const [color,setColor]=useState("olive"); this set backround color to olive 
+  const [color, setColor] = useState(() => {
+    return localStorage.getItem("bgColor") || "olive";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("bgColor", color);
+  }, [color]);
+
+  return (
+    <>
+     <div className="w-full h-screen duration-200"
+      style={{backgroundColor:color}}
+     >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button
+        //passing a calback fun in oncClick
+        onClick={()=>setColor("red")}
+
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"red"}}
+        >Red</button>
+        <button
+        onClick={()=>setColor("green")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"green"}}
+        >Green</button>
+        <button 
+        onClick={()=>setColor("blue")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"blue"}}
+        >Blue</button>
+        {/* <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"red"}}
+        >Red</button> */}
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
